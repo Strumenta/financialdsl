4ab42319e60993cf688fa15fed65070dcdfec6be
Federico Tomassetti <federico@tomassetti.me>
2018-08-16 16:40:57 +0200

    working on first test

--- financialdsl-parser/src/test/kotlin/com/strumenta/financialdsl/ParsingTest.kt
+++ financialdsl-parser/src/test/kotlin/com/strumenta/financialdsl/ParsingTest.kt
@@ -1,7 +1,8 @@
 package com.strumenta.financialdsl
 
 import org.antlr.v4.kotlinruntime.CharStream
-import org.antlr.v4.runtime.CharStreams
+import org.antlr.v4.kotlinruntime.CharStreams
+import org.antlr.v4.kotlinruntime.CommonTokenStream
 import java.nio.file.Path
 import java.nio.file.Paths
 import kotlin.test.Test
@@ -11,8 +12,10 @@ class ParsingTest {
     @Test
     fun parseCompanyTypes() {
         val inputStream = ParsingTest::class.java.getResourceAsStream("/company_types.fin")
+        println("IS $inputStream")
         val input = CharStreams.fromStream(inputStream)
-        val lexer = Financi(input)
-//        var parser = MiniCalcParser(CommonTokenStream(lexer))
+        val lexer = FinancialDSLLexer(input)
+        var parser = FinancialDSLParser(CommonTokenStream(lexer))
+        val root = parser.financialDSLFile()
     }
 }
